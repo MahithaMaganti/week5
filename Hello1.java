f294e963cd7f0dbed2243d57f823361eb2730413
MahithaMaganti <22251a1215@gmail.com>
2025-07-30 10:29:28 +0530

    Update Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Hello1{
 public static void main(String[] args){
-System.out.println("Hello World, WELCOME");
+System.out.println("Hello World, WELCOME TO INDIA");
 }
 }
